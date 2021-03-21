0cd688fb64e05fd2a7f2ecfcd188ea5d2a15dc17
nikodemus1997 <222180028@student.unaki.ac.id>
2021-03-21 18:02:17 +0700

    mengubah tampilan topbar dan button edit

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -38,7 +38,7 @@
             </div>
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Edit</button>
+                    <button type="submit" class="btn-edit btn-primary"><span>Edit Profile</span></button>
                 </div>
             </div>
 
